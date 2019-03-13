8cd5890078c5e25e87a48d41ff4377899a625d94
alvesRenan <renanblack7@hotmail.com>
2019-03-13 14:15:38 -0300

    Updated plot

--- multiple_files.py
+++ multiple_files.py
@@ -2,7 +2,5 @@ import os
 import shlex
 import subprocess as sp
 
-directory = "."
-
 for file in os.listdir("."):
 	sp.call(shlex.split('/home/renan/Documentos/github/script-soma-tempos/read_files.sh %s' % file))
